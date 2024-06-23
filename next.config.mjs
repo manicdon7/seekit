502ad3e6ec751b7a87c3360a2ea041affc4eeb7a
manicdon7 <manicdon7@gmail.com>
2024-06-23 22:24:00 +0530

    deploy

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,19 @@ const nextConfig = {
     images: {
       domains: ['firebasestorage.googleapis.com','lh3.googleusercontent.com','images.remotePatterns'],
     },
+    reactStrictMode: true,
+  webpack: (config) => {
+    config.resolve.fallback = { fs: false, net: false, tls: false };
+    config.externals.push("pino-pretty", "lokijs", "encoding");
+    return config;
+  },
+  typescript: {
+    // !! WARN !!
+    // Dangerously allow production builds to successfully complete even if
+    // your project has type errors.
+    // !! WARN !!
+    ignoreBuildErrors: true,
+  },
   };
   
   export default nextConfig;
