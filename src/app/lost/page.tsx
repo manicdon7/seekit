71760d478bc469bff5bae71d9b4d3a939b13fe46
Vairamuthu M <mvairamuthu2003@gmail.com>
2024-06-24 09:59:33 +0530

    updated the imports

--- src/app/lost/page.tsx
+++ src/app/lost/page.tsx
@@ -1,16 +1,20 @@
-"use client"
-import Navbar from '@/components/Navbar';
-import React, { useState, useEffect } from 'react';
-import { motion } from 'framer-motion';
+"use client";
+import Navbar from "@/Components/Navbar";
+import React, { useState, useEffect } from "react";
+import { motion } from "framer-motion";
 import { FileInput } from "flowbite-react";
-import { getDownloadURL, ref, uploadBytesResumable } from 'firebase/storage';
-import { onAuthStateChanged, signInWithPopup, GoogleAuthProvider } from 'firebase/auth';
-import { storage, auth } from '@/utils/firebase';
+import { getDownloadURL, ref, uploadBytesResumable } from "firebase/storage";
+import {
+  onAuthStateChanged,
+  signInWithPopup,
+  GoogleAuthProvider,
+} from "firebase/auth";
+import { storage, auth } from "@/utils/firebase";
 import Image from "next/image";
-import google from '@/app/assets/google_icon.png';
-import { ToastContainer, toast } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
-import '@/app/globals.css';
+import google from "@/app/assets/google_icon.png";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import "@/app/globals.css";
 
 type FormData = {
   itemName: string;
@@ -28,18 +32,18 @@ type FormData = {
 };
 
 const initialFormData: FormData = {
-  itemName: '',
-  time: '',
-  place: '',
-  category: '',
-  color: '',
-  description: '',
-  imageURL: '',
-  phone: '',
-  address: '',
-  gmail: '',
-  lostEmail: '',
-  founderEmail: '', // Initialize founderEmail with an empty string
+  itemName: "",
+  time: "",
+  place: "",
+  category: "",
+  color: "",
+  description: "",
+  imageURL: "",
+  phone: "",
+  address: "",
+  gmail: "",
+  lostEmail: "",
+  founderEmail: "", // Initialize founderEmail with an empty string
 };
 
 const LostItemForm: React.FC = () => {
@@ -53,7 +57,7 @@ const LostItemForm: React.FC = () => {
         setUserEmail(user.email);
         setFormData((prevData) => ({
           ...prevData,
-          lostEmail: user.email || ""
+          lostEmail: user.email || "",
         }));
       } else {
         setUserEmail(null);
@@ -63,11 +67,15 @@ const LostItemForm: React.FC = () => {
     return () => unsubscribe();
   }, []);
 
-  const handleChange = (e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement | HTMLSelectElement>) => {
+  const handleChange = (
+    e: React.ChangeEvent<
+      HTMLInputElement | HTMLTextAreaElement | HTMLSelectElement
+    >
+  ) => {
     const { name, value } = e.target;
     setFormData({
       ...formData,
-      [name]: value
+      [name]: value,
     });
   };
 
@@ -84,12 +92,12 @@ const LostItemForm: React.FC = () => {
 
         setFormData({
           ...formData,
-          imageURL: downloadURL
+          imageURL: downloadURL,
         });
 
-        console.log('Image uploaded and URL fetched:', downloadURL);
+        console.log("Image uploaded and URL fetched:", downloadURL);
       } catch (error) {
-        console.error('Image upload failed:', error);
+        console.error("Image upload failed:", error);
       }
     }
   };
@@ -105,26 +113,29 @@ const LostItemForm: React.FC = () => {
     console.log(formData);
 
     try {
-      const response = await fetch('https://seekit-server.vercel.app/api/lost-items', {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json'
-        },
-        body: JSON.stringify(formData)
-      });
+      const response = await fetch(
+        "https://seekit-server.vercel.app/api/lost-items",
+        {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify(formData),
+        }
+      );
 
       if (!response.ok) {
-        throw new Error('Network response was not ok');
+        throw new Error("Network response was not ok");
       }
 
       const result = await response.json();
       toast.success("Details Submitted Successfully!");
-      console.log('Lost Item Details:', result);
+      console.log("Lost Item Details:", result);
 
       setFormData(initialFormData);
     } catch (error) {
-      toast.error("Error Submitting Form :(")
-      console.error('Error submitting form:', error);
+      toast.error("Error Submitting Form :(");
+      console.error("Error submitting form:", error);
     }
   };
 
@@ -134,182 +145,218 @@ const LostItemForm: React.FC = () => {
       await signInWithPopup(auth, provider);
       setShowSignInPopup(false);
     } catch (error) {
-      console.error('Error signing in:', error);
+      console.error("Error signing in:", error);
     }
   };
 
   return (
-    <div className='min-h-screen bg-gradient-to-r from-gray-900 to-gray-800 text-white font-anton' id='grid-bg'>
+    <div
+      className="min-h-screen bg-gradient-to-r from-gray-900 to-gray-800 text-white font-anton"
+      id="grid-bg"
+    >
       <Navbar />
       <motion.div
         initial={{ opacity: 0, y: -50 }}
         animate={{ opacity: 1, y: 0 }}
-        transition={{ duration: 0.8, ease: 'easeOut' }}
-        className='container mx-auto p-8'
+        transition={{ duration: 0.8, ease: "easeOut" }}
+        className="container mx-auto p-8"
       >
-        <h2 className='text-3xl font-bold mb-6 text-center pt-20'>Report Lost Item</h2>
-        <form onSubmit={handleSubmit} className='bg-gray-700 p-6 rounded-lg shadow-lg'>
-          <div className='mb-6'>
-            <label htmlFor='itemName' className='block text-gray-300'>Name of Thing</label>
+        <h2 className="text-3xl font-bold mb-6 text-center pt-20">
+          Report Lost Item
+        </h2>
+        <form
+          onSubmit={handleSubmit}
+          className="bg-gray-700 p-6 rounded-lg shadow-lg"
+        >
+          <div className="mb-6">
+            <label htmlFor="itemName" className="block text-gray-300">
+              Name of Thing
+            </label>
             <input
-              type='text'
-              id='itemName'
-              name='itemName'
+              type="text"
+              id="itemName"
+              name="itemName"
               value={formData.itemName}
               onChange={handleChange}
-              className='mt-2 block w-full px-3 py-2 bg-gray-900 border border-gray-600 rounded-md'
+              className="mt-2 block w-full px-3 py-2 bg-gray-900 border border-gray-600 rounded-md"
               required
             />
           </div>
-          <div className='mb-6'>
-            <label htmlFor='time' className='block text-gray-300'>Time Found</label>
+          <div className="mb-6">
+            <label htmlFor="time" className="block text-gray-300">
+              Time Found
+            </label>
             <input
-              type='datetime-local'
-              id='time'
-              name='time'
+              type="datetime-local"
+              id="time"
+              name="time"
               value={formData.time}
               onChange={handleChange}
-              className='mt-2 block w-full px-3 py-2 bg-gray-900 border border-gray-600 rounded-md'
+              className="mt-2 block w-full px-3 py-2 bg-gray-900 border border-gray-600 rounded-md"
               required
             />
           </div>
-          <div className='mb-6'>
-            <label htmlFor='place' className='block text-gray-300'>Place Found</label>
+          <div className="mb-6">
+            <label htmlFor="place" className="block text-gray-300">
+              Place Found
+            </label>
             <input
-              type='text'
-              id='place'
-              name='place'
+              type="text"
+              id="place"
+              name="place"
               value={formData.place}
               onChange={handleChange}
-              className='mt-2 block w-full px-3 py-2 bg-gray-900 border border-gray-600 rounded-md'
+              className="mt-2 block w-full px-3 py-2 bg-gray-900 border border-gray-600 rounded-md"
               required
             />
           </div>
-          <div className='mb-6'>
-            <label htmlFor='category' className='block text-gray-300'>Category</label>
+          <div className="mb-6">
+            <label htmlFor="category" className="block text-gray-300">
+              Category
+            </label>
             <select
-              id='category'
-              name='category'
+              id="category"
+              name="category"
               value={formData.category}
               onChange={handleChange}
-              className='mt-2 block w-full px-3 py-2 bg-gray-900 border border-gray-600 rounded-md'
+              className="mt-2 block w-full px-3 py-2 bg-gray-900 border border-gray-600 rounded-md"
               required
             >
-              <option value='' disabled>Select category</option>
-              <option value='Electronics'>Electronics</option>
-              <option value='Clothing'>Clothing</option>
-              <option value='Accessories'>Accessories</option>
-              <option value='Documents'>Documents</option>
-              <option value='Others'>Others</option>
+              <option value="" disabled>
+                Select category
+              </option>
+              <option value="Electronics">Electronics</option>
+              <option value="Clothing">Clothing</option>
+              <option value="Accessories">Accessories</option>
+              <option value="Documents">Documents</option>
+              <option value="Others">Others</option>
             </select>
           </div>
-          <div className='mb-6'>
-            <label htmlFor='color' className='block text-gray-300'>Color</label>
+          <div className="mb-6">
+            <label htmlFor="color" className="block text-gray-300">
+              Color
+            </label>
             <input
-              type='text'
-              id='color'
-              name='color'
+              type="text"
+              id="color"
+              name="color"
               value={formData.color}
               onChange={handleChange}
-              className='mt-2 block w-full px-3 py-2 bg-gray-900 border border-gray-600 rounded-md'
+              className="mt-2 block w-full px-3 py-2 bg-gray-900 border border-gray-600 rounded-md"
               required
             />
           </div>
-          <div className='mb-6'>
-            <label htmlFor='description' className='block text-gray-300'>Description</label>
+          <div className="mb-6">
+            <label htmlFor="description" className="block text-gray-300">
+              Description
+            </label>
             <textarea
-              id='description'
-              placeholder='give a description about losted thing to help us'
-              name='description'
+              id="description"
+              placeholder="give a description about losted thing to help us"
+              name="description"
               value={formData.description}
               onChange={handleChange}
-              className='mt-2 block w-full px-3 py-2 bg-gray-900 border border-gray-600 rounded-md'
+              className="mt-2 block w-full px-3 py-2 bg-gray-900 border border-gray-600 rounded-md"
               rows={3}
               required
             />
           </div>
-          <div className='mb-6'>
-            <label htmlFor='image' className='block text-gray-300'>Upload Image (optional)</label>
+          <div className="mb-6">
+            <label htmlFor="image" className="block text-gray-300">
+              Upload Image (optional)
+            </label>
             <div>
               <FileInput
-                id='file-upload'
+                id="file-upload"
                 onChange={handleFileChange}
-                className='mt-2 block w-full px-3 py-2 bg-gray-900 border border-gray-600 rounded-md'
+                className="mt-2 block w-full px-3 py-2 bg-gray-900 border border-gray-600 rounded-md"
               />
             </div>
           </div>
-          <div className='mb-6'>
-            <label htmlFor='phone' className='block text-gray-300'>Phone Number</label>
+          <div className="mb-6">
+            <label htmlFor="phone" className="block text-gray-300">
+              Phone Number
+            </label>
             <input
-              type='text'
-              id='phone'
-              name='phone'
+              type="text"
+              id="phone"
+              name="phone"
               value={formData.phone}
               onChange={handleChange}
-              className='mt-2 block w-full px-3 py-2 bg-gray-900 border border-gray-600 rounded-md'
+              className="mt-2 block w-full px-3 py-2 bg-gray-900 border border-gray-600 rounded-md"
               required
             />
           </div>
-          <div className='mb-6'>
-            <label htmlFor='address' className='block text-gray-300'>Address</label>
+          <div className="mb-6">
+            <label htmlFor="address" className="block text-gray-300">
+              Address
+            </label>
             <input
-              type='text'
-              id='address'
-              name='address'
+              type="text"
+              id="address"
+              name="address"
               value={formData.address}
               onChange={handleChange}
-              className='mt-2 block w-full px-3 py-2 bg-gray-900 border border-gray-600 rounded-md'
+              className="mt-2 block w-full px-3 py-2 bg-gray-900 border border-gray-600 rounded-md"
               required
             />
           </div>
-          <div className='mb-6'>
-            <label htmlFor='gmail' className='block text-gray-300'>Gmail</label>
+          <div className="mb-6">
+            <label htmlFor="gmail" className="block text-gray-300">
+              Gmail
+            </label>
             <input
-              type='email'
-              id='gmail'
-              name='gmail'
+              type="email"
+              id="gmail"
+              name="gmail"
               value={formData.gmail}
               onChange={handleChange}
-              className='mt-2 block w-full px-3 py-2 bg-gray-900 border border-gray-600 rounded-md'
+              className="mt-2 block w-full px-3 py-2 bg-gray-900 border border-gray-600 rounded-md"
             />
           </div>
           <input
-            type='hidden'
-            name='founderEmail'
+            type="hidden"
+            name="founderEmail"
             value={formData.founderEmail}
           />
           <button
-            type='submit'
-            className='w-full py-3 mt-4 bg-[#4ECCA3] rounded-md hover:bg-emerald-500 transition-colors'
+            type="submit"
+            className="w-full py-3 mt-4 bg-[#4ECCA3] rounded-md hover:bg-emerald-500 transition-colors"
           >
             Submit
           </button>
         </form>
       </motion.div>
       {showSignInPopup && (
-        <div className='fixed inset-0 bg-gray-800 bg-opacity-75 flex items-center justify-center z-50'>
-          <div className='bg-gray-700 p-8 rounded-lg shadow-lg text-center'>
-            <h3 className='text-3xl mb-4 font-bold text-white'>Sign In Required</h3>
-            <hr className='border-gray-500 mb-6' />
-            <p className='text-gray-300 mb-6'>To submit the form, please sign in with your Google account. This helps us to verify the information and contact you if necessary.</p>
+        <div className="fixed inset-0 bg-gray-800 bg-opacity-75 flex items-center justify-center z-50">
+          <div className="bg-gray-700 p-8 rounded-lg shadow-lg text-center">
+            <h3 className="text-3xl mb-4 font-bold text-white">
+              Sign In Required
+            </h3>
+            <hr className="border-gray-500 mb-6" />
+            <p className="text-gray-300 mb-6">
+              To submit the form, please sign in with your Google account. This
+              helps us to verify the information and contact you if necessary.
+            </p>
             <button
               onClick={handleSignIn}
-              className='py-2 px-4 flex justify-center bg-[#4ECCA3] rounded-md text-white hover:bg-emerald-500 transition-colors'
+              className="py-2 px-4 flex justify-center bg-[#4ECCA3] rounded-md text-white hover:bg-emerald-500 transition-colors"
             >
-              <p>Sign in with Google</p> <Image src={google} alt='google' width={30} height={30} />
+              <p>Sign in with Google</p>{" "}
+              <Image src={google} alt="google" width={30} height={30} />
             </button>
           </div>
         </div>
       )}
       <ToastContainer
-        position='top-right'
+        position="top-right"
         autoClose={5000}
         hideProgressBar={false}
         newestOnTop={false}
         closeOnClick
         rtl={false}
         pauseOnFocusLoss
-        theme='colored'
+        theme="colored"
       />
     </div>
   );
