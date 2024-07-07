df9a58ae3832f5a26baff9b3ec32462772dcacad
manicdon7 <manicdon7@gmail.com>
2024-07-07 22:33:59 +0530

    Faq and Seek minor changes

--- src/Components/FAQ.tsx
+++ src/Components/FAQ.tsx
@@ -3,6 +3,7 @@ import { useEffect } from 'react';
 import '@/app/globals.css';
 import Aos from 'aos';
 import 'aos/dist/aos.css';
+import Link from 'next/link';
 
 const FAQ: React.FC = () => {
     useEffect(() => {
@@ -52,57 +53,63 @@ const FAQ: React.FC = () => {
             </div>
 
             <div className='flex gap-8 space-x-16 justify-center'>
-            <div className="card">
-                <div className="bg">
-                    <div className='flex justify-center pt-10'>
-                        <svg xmlns="http://www.w3.org/2000/svg" width="70" height="70" viewBox="0 0 36 36"><path fill="white" d="M30.86 20.94a4.7 4.7 0 0 1 1.86.64h.05a15.1 15.1 0 0 0-.61-8.37a1 1 0 1 0-1.87.69a13.2 13.2 0 0 1 .57 7.04m-4.53 7.64a13 13 0 0 1-6.07 2.82a1 1 0 1 0 .17 2h.18a15.16 15.16 0 0 0 7.21-3.4v-.07a4.7 4.7 0 0 1-1.49-1.35m-10.71 2.76a13.3 13.3 0 0 1-4.29-1.61a15 15 0 0 1-1.63-1.11A4.7 4.7 0 0 1 8.24 30a16 16 0 0 0 2.07 1.48a15.4 15.4 0 0 0 4.94 1.86h.19a1 1 0 0 0 .18-2M4.56 21.15q.3 0 .6-.09A13 13 0 0 1 5.7 14a1 1 0 0 0-1.88-.69a15 15 0 0 0-.56 8.43a4.8 4.8 0 0 1 1.3-.59" /><path fill="white" d="M31.9 23a3.2 3.2 0 0 0-2.43-.42a3.3 3.3 0 0 0-1.4.77l-3.87-2.24a6.87 6.87 0 0 0-2.77-8.43l-.11-.07a6.7 6.7 0 0 0-2.42-.81V8a3.23 3.23 0 0 0 1.88-1.5A3.3 3.3 0 0 0 19.65 2a3.15 3.15 0 0 0-2.42-.32a3.24 3.24 0 0 0-2 1.51a3.3 3.3 0 0 0 1.13 4.46a3 3 0 0 0 .74.35v3.8a6.63 6.63 0 0 0-4.86 3.28a6.85 6.85 0 0 0-.42 6l-4 2.29a4 4 0 0 0-.45-.37A3.2 3.2 0 0 0 3 24.21a3.3 3.3 0 0 0 1.1 4.46a3.2 3.2 0 0 0 1.65.46a3 3 0 0 0 .78-.1a3.25 3.25 0 0 0 2.34-3.94v-.17l3.88-2.24a7 7 0 0 0 1.89 1.71a6.49 6.49 0 0 0 8.73-1.7l3.83 2.21a3.29 3.29 0 0 0 1.45 3.64A3.18 3.18 0 0 0 33 27.41A3.3 3.3 0 0 0 31.9 23M8.05 10a13 13 0 0 1 5.35-3.77a5 5 0 0 1-.17-2.07a15.15 15.15 0 0 0-6.7 4.51A1 1 0 0 0 8.05 10" /><path fill="white" d="M24.67 7.23A13.1 13.1 0 0 1 27.93 10a1 1 0 1 0 1.52-1.3a15 15 0 0 0-3.76-3.2a16 16 0 0 0-2.94-1.33a4.8 4.8 0 0 1-.15 2.06a14 14 0 0 1 2.07 1" /><path fill="none" d="M0 0h36v36H0z" /></svg>
-                    </div>
-                    <div className='pt-10 flex justify-evenly px-2'>
-                        <div>
-                        <h1 className='text-lg text-white font-medium text-justify'>How to Post Founded Things in Seekit ? </h1> 
+                <div className="card">
+                    <div className="bg">
+                        <div className='flex justify-center pt-10'>
+                            <svg xmlns="http://www.w3.org/2000/svg" width="70" height="70" viewBox="0 0 36 36"><path fill="white" d="M30.86 20.94a4.7 4.7 0 0 1 1.86.64h.05a15.1 15.1 0 0 0-.61-8.37a1 1 0 1 0-1.87.69a13.2 13.2 0 0 1 .57 7.04m-4.53 7.64a13 13 0 0 1-6.07 2.82a1 1 0 1 0 .17 2h.18a15.16 15.16 0 0 0 7.21-3.4v-.07a4.7 4.7 0 0 1-1.49-1.35m-10.71 2.76a13.3 13.3 0 0 1-4.29-1.61a15 15 0 0 1-1.63-1.11A4.7 4.7 0 0 1 8.24 30a16 16 0 0 0 2.07 1.48a15.4 15.4 0 0 0 4.94 1.86h.19a1 1 0 0 0 .18-2M4.56 21.15q.3 0 .6-.09A13 13 0 0 1 5.7 14a1 1 0 0 0-1.88-.69a15 15 0 0 0-.56 8.43a4.8 4.8 0 0 1 1.3-.59" /><path fill="white" d="M31.9 23a3.2 3.2 0 0 0-2.43-.42a3.3 3.3 0 0 0-1.4.77l-3.87-2.24a6.87 6.87 0 0 0-2.77-8.43l-.11-.07a6.7 6.7 0 0 0-2.42-.81V8a3.23 3.23 0 0 0 1.88-1.5A3.3 3.3 0 0 0 19.65 2a3.15 3.15 0 0 0-2.42-.32a3.24 3.24 0 0 0-2 1.51a3.3 3.3 0 0 0 1.13 4.46a3 3 0 0 0 .74.35v3.8a6.63 6.63 0 0 0-4.86 3.28a6.85 6.85 0 0 0-.42 6l-4 2.29a4 4 0 0 0-.45-.37A3.2 3.2 0 0 0 3 24.21a3.3 3.3 0 0 0 1.1 4.46a3.2 3.2 0 0 0 1.65.46a3 3 0 0 0 .78-.1a3.25 3.25 0 0 0 2.34-3.94v-.17l3.88-2.24a7 7 0 0 0 1.89 1.71a6.49 6.49 0 0 0 8.73-1.7l3.83 2.21a3.29 3.29 0 0 0 1.45 3.64A3.18 3.18 0 0 0 33 27.41A3.3 3.3 0 0 0 31.9 23M8.05 10a13 13 0 0 1 5.35-3.77a5 5 0 0 1-.17-2.07a15.15 15.15 0 0 0-6.7 4.51A1 1 0 0 0 8.05 10" /><path fill="white" d="M24.67 7.23A13.1 13.1 0 0 1 27.93 10a1 1 0 1 0 1.52-1.3a15 15 0 0 0-3.76-3.2a16 16 0 0 0-2.94-1.33a4.8 4.8 0 0 1-.15 2.06a14 14 0 0 1 2.07 1" /><path fill="none" d="M0 0h36v36H0z" /></svg>
                         </div>
-                        <div className='pt-16'>
-                        <svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 8 8"><path fill="white" d="M5 1v2H0v1h5v2l3-2.53z"/></svg> 
+                        <div className='pt-10 flex justify-evenly px-2'>
+                            <div>
+                                <h1 className='text-lg text-white font-medium text-justify'>Where to Post Founded Things in Seekit ? </h1>
+                            </div>
+                            <Link href="/found">
+                                <div className='pt-16'>
+                                    <svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 8 8"><path fill="white" d="M5 1v2H0v1h5v2l3-2.53z" /></svg>
+                                </div>
+                            </Link>
                         </div>
                     </div>
+                    <div className="blob"></div>
                 </div>
-                <div className="blob"></div>
-            </div>
-            <div className="card">
-                <div className="bg">
-                    <div className='flex justify-center pt-10'>
-                    <svg xmlns="http://www.w3.org/2000/svg" width="70" height="70" viewBox="0 0 14 14"><path fill="white" fill-rule="evenodd" d="M5.763 2.263A1.75 1.75 0 0 1 8.75 3.5h-3.5c0-.464.184-.91.513-1.237M3.75 3.5a3.25 3.25 0 0 1 6.5 0h1.25A2.5 2.5 0 0 1 14 6v5.5a2.5 2.5 0 0 1-2.5 2.5h-9A2.5 2.5 0 0 1 0 11.5V6a2.5 2.5 0 0 1 2.5-2.5zm2.915 3.067A.875.875 0 1 1 7 8.25a.625.625 0 0 0-.625.625v1a.625.625 0 1 0 1.25 0v-.469a2.125 2.125 0 1 0-2.75-2.031a.625.625 0 1 0 1.25 0a.875.875 0 0 1 .54-.808m.337 6.308a.75.75 0 1 1 0-1.5a.75.75 0 0 1 0 1.5" clip-rule="evenodd" /></svg>
-                        
-                    </div>
-                    <div className='pt-10 flex justify-evenly px-2'>
-                        <div>
-                        <h1 className='text-lg text-white font-medium text-justify'>How to Post Losted Things in Seekit ? </h1> 
+                <div className="card">
+                    <div className="bg">
+                        <div className='flex justify-center pt-10'>
+                            <svg xmlns="http://www.w3.org/2000/svg" width="70" height="70" viewBox="0 0 14 14"><path fill="white" fill-rule="evenodd" d="M5.763 2.263A1.75 1.75 0 0 1 8.75 3.5h-3.5c0-.464.184-.91.513-1.237M3.75 3.5a3.25 3.25 0 0 1 6.5 0h1.25A2.5 2.5 0 0 1 14 6v5.5a2.5 2.5 0 0 1-2.5 2.5h-9A2.5 2.5 0 0 1 0 11.5V6a2.5 2.5 0 0 1 2.5-2.5zm2.915 3.067A.875.875 0 1 1 7 8.25a.625.625 0 0 0-.625.625v1a.625.625 0 1 0 1.25 0v-.469a2.125 2.125 0 1 0-2.75-2.031a.625.625 0 1 0 1.25 0a.875.875 0 0 1 .54-.808m.337 6.308a.75.75 0 1 1 0-1.5a.75.75 0 0 1 0 1.5" clip-rule="evenodd" /></svg>
+
                         </div>
-                        <div className='pt-16'>
-                        <svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 8 8"><path fill="white" d="M5 1v2H0v1h5v2l3-2.53z"/></svg> 
+                        <div className='pt-10 flex justify-evenly px-2'>
+                            <div>
+                                <h1 className='text-lg text-white font-medium text-justify'>Where to Post Losted Things in Seekit ? </h1>
+                            </div>
+                            <Link href="/lost">
+                                <div className='pt-16'>
+                                    <svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 8 8"><path fill="white" d="M5 1v2H0v1h5v2l3-2.53z" /></svg>
+                                </div>
+                            </Link>
                         </div>
                     </div>
+                    <div className="blob"></div>
                 </div>
-                <div className="blob"></div>
-            </div>
-            <div className="card">
-                <div className="bg">
-                    <div className='flex justify-center pt-10'>
-                    <svg xmlns="http://www.w3.org/2000/svg" width="70" height="70" viewBox="0 0 24 24"><path fill="none" stroke="white" stroke-width="2" d="M18.5 21a4.5 4.5 0 1 0 0-9a4.5 4.5 0 0 0 0 9ZM10 7h4M1.5 14.5S5.5 5 6 4s1.5-1 2-1s2 0 2 2v11m-4.5 5a4.5 4.5 0 1 1 0-9a4.5 4.5 0 0 1 0 9Zm17-6.5S18.5 5 18 4s-1.5-1-2-1s-2 0-2 2v11m-4 0h4" /></svg>
-                    </div>
-                    <div className='pt-10 flex justify-evenly px-2'>
-                        <div>
-                        <h1 className='text-lg text-white font-medium text-justify'>How to Seek Things in Seekit ? </h1> 
+                <div className="card">
+                    <div className="bg">
+                        <div className='flex justify-center pt-10'>
+                            <svg xmlns="http://www.w3.org/2000/svg" width="70" height="70" viewBox="0 0 24 24"><path fill="none" stroke="white" stroke-width="2" d="M18.5 21a4.5 4.5 0 1 0 0-9a4.5 4.5 0 0 0 0 9ZM10 7h4M1.5 14.5S5.5 5 6 4s1.5-1 2-1s2 0 2 2v11m-4.5 5a4.5 4.5 0 1 1 0-9a4.5 4.5 0 0 1 0 9Zm17-6.5S18.5 5 18 4s-1.5-1-2-1s-2 0-2 2v11m-4 0h4" /></svg>
                         </div>
-                        <div className='pt-16'>
-                        <svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 8 8"><path fill="white" d="M5 1v2H0v1h5v2l3-2.53z"/></svg> 
+                        <div className='pt-10 flex justify-evenly px-2'>
+                            <div>
+                                <h1 className='text-lg text-white font-medium text-justify'>where to Seek Things in Seekit ? </h1>
+                            </div>
+                            <Link href="/seek">
+                                <div className='pt-16'>
+                                    <svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 8 8"><path fill="white" d="M5 1v2H0v1h5v2l3-2.53z" /></svg>
+                                </div>
+                            </Link>
                         </div>
                     </div>
+                    <div className="blob"></div>
                 </div>
-                <div className="blob"></div>
             </div>
-            </div>
-            
+
         </div>
     )
 }
--- src/app/seek/page.tsx
+++ src/app/seek/page.tsx
@@ -32,6 +32,128 @@ type LostItem = {
   imageURL?: string;
 };
 
+const FoundItemCard: React.FC<{ item: FoundItem }> = ({ item }) => (
+  <div key={item._id} className="bg-gray-700 p-6 rounded-lg shadow-lg flex flex-col mb-4">
+    {item.imageURL && (
+      <div className="mb-4 rounded-lg overflow-hidden h-64 w-full">
+        <Image
+          src={item.imageURL}
+          alt={item.itemName}
+          layout="responsive"
+          width={500}
+          height={300}
+          objectFit="cover"
+        />
+      </div>
+    )}
+
+    <div className="flex flex-col flex-grow">
+      <h3 className="text-2xl font-bold mb-2">{item.itemName}</h3>
+      <hr className="border-gray-500 mb-4" />
+      <div className="mb-4">
+        <p>
+          <span className="font-bold">Category:</span> {item.category}
+        </p>
+        <p>
+          <span className="font-bold">Color:</span> {item.color}
+        </p>
+      </div>
+      <div className="mb-4 flex-grow">
+        <p>
+          <span className="font-bold">Description:</span> {item.description}
+        </p>
+      </div>
+      <div className="mb-4">
+        <p>
+          <span className="font-bold">Time Found:</span>{" "}
+          {new Date(item.time).toLocaleString()}
+        </p>
+        <p>
+          <span className="font-bold">Place Found:</span> {item.place}
+        </p>
+      </div>
+      <div className="mb-4">
+        <p>
+          <span className="font-bold">Contact:</span> {item.phone}
+        </p>
+        <p>
+          <span className="font-bold">Address:</span> {item.address}
+        </p>
+        <p>
+          <span className="font-bold">Email:</span> {item.gmail}
+        </p>
+      </div>
+      <a
+        href={`mailto:${item.gmail}`}
+        className="block mt-4 py-2 px-4 bg-blue-600 rounded-md text-center hover:bg-blue-700 transition-colors"
+      >
+        Contact via Email
+      </a>
+    </div>
+  </div>
+);
+
+const LostItemCard: React.FC<{ item: LostItem }> = ({ item }) => (
+  <div key={item._id} className="bg-gray-700 p-6 rounded-lg shadow-lg flex flex-col mb-4">
+    {item.imageURL && (
+      <div className="mb-4 rounded-lg overflow-hidden h-64 w-full">
+        <Image
+          src={item.imageURL}
+          alt={item.itemName}
+          layout="responsive"
+          width={500}
+          height={300}
+          objectFit="cover"
+        />
+      </div>
+    )}
+
+    <div className="flex flex-col flex-grow">
+      <h3 className="text-2xl font-bold mb-2">{item.itemName}</h3>
+      <hr className="border-gray-500 mb-4" />
+      <div className="mb-4">
+        <p>
+          <span className="font-bold">Category:</span> {item.category}
+        </p>
+        <p>
+          <span className="font-bold">Color:</span> {item.color}
+        </p>
+      </div>
+      <div className="mb-4 flex-grow">
+        <p>
+          <span className="font-bold">Description:</span> {item.description}
+        </p>
+      </div>
+      <div className="mb-4">
+        <p>
+          <span className="font-bold">Time Lost:</span>{" "}
+          {new Date(item.time).toLocaleString()}
+        </p>
+        <p>
+          <span className="font-bold">Place Lost:</span> {item.place}
+        </p>
+      </div>
+      <div className="mb-4">
+        <p>
+          <span className="font-bold">Contact:</span> {item.phone}
+        </p>
+        <p>
+          <span className="font-bold">Address:</span> {item.address}
+        </p>
+        <p>
+          <span className="font-bold">Email:</span> {item.gmail}
+        </p>
+      </div>
+      <a
+        href={`mailto:${item.gmail}`}
+        className="block mt-4 py-2 px-4 bg-blue-600 rounded-md text-center hover:bg-blue-700 transition-colors"
+      >
+        Contact via Email
+      </a>
+    </div>
+  </div>
+);
+
 const SeekPage: React.FC = () => {
   const [foundItems, setFoundItems] = useState<FoundItem[]>([]);
   const [lostItems, setLostItems] = useState<LostItem[]>([]);
@@ -61,7 +183,6 @@ const SeekPage: React.FC = () => {
           "https://seekit-server.vercel.app/api/lost-items"
         );
         const data = await response.json();
-        console.log(data);
         setLostItems(data);
       } catch (error) {
         console.error("Error fetching lost items:", error);
@@ -78,86 +199,28 @@ const SeekPage: React.FC = () => {
       item.category.toLowerCase().includes(searchTerm.toLowerCase())
   );
 
-  // Check if lostItems is an array before filtering
   const filteredLostItems = Array.isArray(lostItems)
     ? lostItems.filter(
-      (item) =>
-        item.itemName.toLowerCase().includes(searchTerm.toLowerCase()) ||
-        item.place.toLowerCase().includes(searchTerm.toLowerCase()) ||
-        item.category.toLowerCase().includes(searchTerm.toLowerCase())
-    )
+        (item) =>
+          item.itemName.toLowerCase().includes(searchTerm.toLowerCase()) ||
+          item.place.toLowerCase().includes(searchTerm.toLowerCase()) ||
+          item.category.toLowerCase().includes(searchTerm.toLowerCase())
+      )
     : [];
 
-  const renderItems = (items: (FoundItem | LostItem)[]) => {
-    return items.length > 0 ? (
-      items.map((item) => (
-        <div
-          key={item._id}
-          className="bg-gray-700 p-6 rounded-lg shadow-lg flex flex-col mb-4"
-        >
-          {item.imageURL && (
-            <div className="mb-4 rounded-lg overflow-hidden h-64 w-full">
-              <Image
-                src={item.imageURL}
-                alt={item.itemName}
-                layout="responsive"
-                width={500} // Adjust the width according to your design
-                height={300} // Adjust the height according to your design
-                objectFit="cover"
-              />
-            </div>
-          )}
-
-          <div className="flex flex-col flex-grow">
-            <h3 className="text-2xl font-bold mb-2">{item.itemName}</h3>
-            <hr className="border-gray-500 mb-4" />
-            <div className="mb-4">
-              <p>
-                <span className="font-bold">Category:</span> {item.category}
-              </p>
-              <p>
-                <span className="font-bold">Color:</span> {item.color}
-              </p>
-            </div>
-            <div className="mb-4 flex-grow">
-              <p>
-                <span className="font-bold">Description:</span>{" "}
-                {item.description}
-              </p>
-            </div>
-            <div className="mb-4">
-              <p>
-                <span className="font-bold">Time Found:</span>{" "}
-                {new Date(item.time).toLocaleString()}
-              </p>
-              <p>
-                <span className="font-bold">Place Found:</span> {item.place}
-              </p>
-            </div>
-            <div className="mb-4">
-              <p>
-                <span className="font-bold">Contact:</span> {item.phone}
-              </p>
-              <p>
-                <span className="font-bold">Address:</span> {item.address}
-              </p>
-              <p>
-                <span className="font-bold">Email:</span> {item.gmail}
-              </p>
-            </div>
-            <a
-              href={`mailto:${item.gmail}`}
-              className="block mt-4 py-2 px-4 bg-blue-600 rounded-md text-center hover:bg-blue-700 transition-colors"
-            >
-              Contact via Email
-            </a>
-          </div>
-        </div>
-      ))
+  const renderFoundItems = () => {
+    return filteredFoundItems.length > 0 ? (
+      filteredFoundItems.map((item) => <FoundItemCard key={item._id} item={item} />)
     ) : (
-      <p className="text-center w-full">
-        No items found matching your search criteria.
-      </p>
+      <p className="text-center w-full">No items found matching your search criteria.</p>
+    );
+  };
+
+  const renderLostItems = () => {
+    return filteredLostItems.length > 0 ? (
+      filteredLostItems.map((item) => <LostItemCard key={item._id} item={item} />)
+    ) : (
+      <p className="text-center w-full">No items found matching your search criteria.</p>
     );
   };
 
@@ -167,8 +230,7 @@ const SeekPage: React.FC = () => {
         <title>{showFoundItems ? "Found Items" : "Lost Items"} - Seekit</title>
         <meta
           name="description"
-          content={`Explore ${showFoundItems ? "found" : "lost"
-            } items shared by the community on Seekit.`}
+          content={`Explore ${showFoundItems ? "found" : "lost"} items shared by the community on Seekit.`}
         />
         <meta
           property="og:title"
@@ -176,14 +238,13 @@ const SeekPage: React.FC = () => {
         />
         <meta
           property="og:description"
-          content={`Explore ${showFoundItems ? "found" : "lost"
-            } items shared by the community on Seekit.`}
+          content={`Explore ${showFoundItems ? "found" : "lost"} items shared by the community on Seekit.`}
         />
         <meta
           property="og:image"
           content={
             (showFoundItems && filteredFoundItems.length > 0) ||
-              (!showFoundItems && filteredLostItems.length > 0)
+            (!showFoundItems && filteredLostItems.length > 0)
               ? showFoundItems
                 ? filteredFoundItems[0].imageURL
                 : filteredLostItems[0].imageURL
@@ -210,22 +271,24 @@ const SeekPage: React.FC = () => {
         </div>
         <div className="flex justify-center mb-6 bg-gray-800 p-5 rounded-full gap-10 shadow-md overflow-hidden">
           <button
-            className={`flex-1 px-6 py-3 rounded-full text-2xl font-semibold ${showFoundItems ? "bg-[#4ECCA3]" : "bg-gray-600"
-              } text-white hover:bg-teal-500 transition-colors focus:outline-none`}
+            className={`flex-1 px-6 py-3 rounded-full text-2xl font-semibold ${
+              showFoundItems ? "bg-[#4ECCA3]" : "bg-gray-600"
+            } text-white hover:bg-teal-500 transition-colors focus:outline-none`}
             onClick={() => setShowFoundItems(true)}
           >
             Found Items
           </button>
           <button
-            className={`flex-1 px-6 py-3 rounded-full text-2xl font-semibold ${!showFoundItems ? "bg-[#4ECCA3]" : "bg-gray-600"
-              } text-white hover:bg-teal-500 transition-colors focus:outline-none`}
+            className={`flex-1 px-6 py-3 rounded-full text-2xl font-semibold ${
+              !showFoundItems ? "bg-[#4ECCA3]" : "bg-gray-600"
+            } text-white hover:bg-teal-500 transition-colors focus:outline-none`}
             onClick={() => setShowFoundItems(false)}
           >
             Lost Items
           </button>
         </div>
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
-          {showFoundItems ? renderItems(filteredFoundItems) : renderItems(filteredLostItems)}
+          {showFoundItems ? renderFoundItems() : renderLostItems()}
         </div>
       </div>
       <footer className="bg-gray-800 p-4 text-center text-gray-400 mt-8">
