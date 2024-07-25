a28ca9b840828ec34484e0de3576b06d7d75594a
SiriChandhana06 <sirisiri3006@gmail.com>
2024-07-25 21:54:12 +0530

    signup backend

--- Server/models/User.js
+++ Server/models/User.js
@@ -1,9 +1,8 @@
 const mongoose = require('mongoose');
 
 const UserSchema = new mongoose.Schema({
-    gmail: { type: String },
-    Password: {type: String}
+    gmail: { type: String, unique: true },
+    password: { type: String }
 });
 
 module.exports = mongoose.model('User', UserSchema);
-
