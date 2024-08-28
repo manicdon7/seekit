fc2797f2fe5dcdbf24672c7847a6eb95e01cb1e8
manicdon7 <manicdon7@gmail.com>
2024-08-28 11:51:45 +0530

    url

--- Server/routes/allposts.js
+++ Server/routes/allposts.js
@@ -3,19 +3,19 @@ const FoundItem = require('../models/founditems');
 
 const router = express.Router();
 
-router.get('/myposts/:postId', async (req, res) => {
+router.get('/:postId', async (req, res) => {
   const { postId } = req.params;
   console.log(postId); // Logging the postId to verify it's received correctly
 
   try {
     const post = await FoundItem.findById(postId);
-    console.log(post);
+    console.log(post); // Logging the retrieved post to verify
 
     if (!post) {
       return res.status(404).json({ error: 'Post not found' });
     }
 
-    const postUrl = `https://seekit.vercel.app/myposts/myposts?postId=${postId}`;
+    const postUrl = `https://seekit.vercel.app/myposts/${postId}`;
     res.status(200).json({ post, postUrl });
   } catch (error) {
     console.error('Error fetching post:', error);
